65e85c989e303b644760c5c944ca8323add8c0ba
Danail <danail.iordanov@yahoo.com>
2020-04-05 22:41:01 +0200

    Uploaded Exercise 1

---
+++ Exercises/01_lexicalFoundation/01_lexicalFoundation.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n"; // This is a single-line comment
+
+    /*
+    This
+    is
+    a
+    multi-line
+    comment
+    */
+
+    const string myMultiLineString = "This is a \
+multi-line \
+string \
+with a\t<- tabulation \
+and a\nnew line";
+    
+    // definition
+    double speedOfLight, pi, conversionFactorDegreeToGon;
+
+    // initialization
+    speedOfLight = 2.99792458e8; // meters / second
+    pi = 3.1415926535897932385;
+    conversionFactorDegreeToGon = 1.1111111111;
+
+    // printing on the console
+    cout << myMultiLineString << endl;
+    cout << speedOfLight << endl;
+    cout << pi << endl;
+    cout << conversionFactorDegreeToGon << endl;
+
+    cout << "Decimal: " << 10 << endl; //10
+    cout << "Hexadecimal: " << 0xAFFE << endl; // 45054
+    cout << "Binary: " << 0b11011 << endl; // 27
+    cout << "Octal: " << 04711 << endl; // 2505
+    // There is no pre-defined literal for a number with base 4
+
+    char hashtag = 35;
+    cout << hashtag; // #
+}
